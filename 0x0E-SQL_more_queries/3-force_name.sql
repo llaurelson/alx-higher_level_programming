dbcdb62e2707e2f3be21b20a1ca891998e7d6d2f
llaurelson <llaurelson@gmail.com>
2023-02-16 03:50:12 +0000

    more sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,1 @@
+-- a script that creates the table force_name on your MySQL server.
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
