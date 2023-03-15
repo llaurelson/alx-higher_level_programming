fb2a7e2c521faea9e9cb0c6f30b165727a47646e
llaurelson <llaurelson@gmail.com>
2023-03-15 11:34:32 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+
+const num = parseInt(process.argv[2], 10);
+if (isNaN(num)) {
+  console.log('Missing number of occurences');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+  for (let i = num; i > 0; i -= 1) {
     console.log('C is fun');
-    i++;
   }
 }
-
