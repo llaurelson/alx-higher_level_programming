fcb8f568facf4141e764190e71dac20cce2c1a4b
llaurelson <llaurelson@gmail.com>
2023-03-15 12:35:26 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module 9-add_item
+Module 7-add_item
 
 Contains function that adds and saves to Python obj to JSON file; loads objects
 
@@ -17,8 +17,8 @@ Contains function that adds and saves to Python obj to JSON file; loads objects
 
 
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
