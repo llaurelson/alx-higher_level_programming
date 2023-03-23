a91e9d11daef21e91e8605ff79d4fa7d7d05c20b
llaurelson <llaurelson@gmail.com>
2023-03-23 07:31:33 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,16 +7,19 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let col = 0; col < this.height; col += 1) {
       console.log('X'.repeat(this.width));
     }
   }
+  
   rotate () {
     let tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
+  
   double () {
     this.width *= 2;
     this.height *= 2;
