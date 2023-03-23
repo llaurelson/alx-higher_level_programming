bc14e7ff5e7e0832e4ba29caeb3d31dec963adaf
llaurelson <llaurelson@gmail.com>
2023-03-23 19:26:31 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,55 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
+class Rectangle {
+
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+
+    if ((w > 0) && (h > 0)) {
+
       this.width = w;
+
       this.height = h;
+
     }
+
   }
 
   print () {
-    for (let col = 0; col < this.height; col += 1) {
-      console.log('X'.repeat(this.width));
+
+    for (let i = 0; i < this.height; i++) {
+
+      let s = '';
+
+      for (let j = 0; j < this.width; j++) {
+
+        s += 'X';
+
+      }
+
+      console.log(s);
+
     }
+
   }
-  
+
   rotate () {
-    const tmp = this.width;
+
+    const aux = this.width;
+
     this.width = this.height;
-    this.height = tmp;
+
+    this.height = aux;
+
   }
-  
+
   double () {
+
     this.width *= 2;
+
     this.height *= 2;
+
   }
-};
+
+}
+
+module.exports = Rectangle;
