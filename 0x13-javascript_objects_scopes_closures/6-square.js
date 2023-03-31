4d258af4b0d6c8e0d19b30c8b493e7cf6762e016
llaurelson <llaurelson@gmail.com>
2023-03-31 13:11:52 +0000

    updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
-const BaseSquare = require('./5-square');
+const SquareP = require('./5-square');
 
-module.exports = class Square extends BaseSquare {
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let col = 0; col < this.width; col += 1) {
-        console.log(c.repeat(this.height));
-      };
-    };
-  };
-};
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
