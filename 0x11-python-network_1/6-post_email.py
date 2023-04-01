60937ec329ee267a104e0f1e54ed4763fd3e8577
llaurelson <llaurelson@gmail.com>
2023-04-01 10:17:36 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+
 """
-import sys
-import urllib.request
+
+given URL & email as params, send POST req to URL, display response body utf-8
+
+usage: ./6-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
+
+"""
+
+from sys import argv
+
+import requests
 
 if __name__ == "__main__":
-    url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    url = argv[1]
+
+    payload = {'email': argv[2]}
+
+    r = requests.post(url, data=payload)
+
+    print(r.text)
