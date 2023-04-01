ebf2c603269d486b0c078a329730f46eef6b0d52
llaurelson <llaurelson@gmail.com>
2023-04-01 06:37:10 +0000

    updated

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-if [[ $# -eq 0 ]]; then
-
-  echo "Usage: $0 URL"
-
-  exit 1
-
-fi
-
-url=$1
-
-curl -H "X-School-User-Id: 98" $url
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
