da73c14a73cbc0146f25edacb9a933a6ac2de691
llaurelson <llaurelson@gmail.com>
2023-04-03 08:19:43 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+fetches https://intranet.hbtn.io/status; display response 
+"""
 import requests
-
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
