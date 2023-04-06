cea6da2e5b5e303aa9ffab6fe07d6a92ceca23cb
llaurelson <llaurelson@gmail.com>
2023-04-06 15:07:09 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,49 +1,91 @@
-#!/usr/bin/python3
-"""
-Script reads stdin line by line and computes metrics
+#!/usr/bin/python3ads from standard input and computes metrics.
 
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-
-Each 10 lines and after a keyboard interruption (CTRL + C),
-prints those statistics since the beginning:
-total file size and
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-
-format: File size: <total size>
-format: <status code (in ascending order)>: <number>
 """
 
+After every ten lines or the input of a keyboard interruption (CTRL + C),
 
-import sys
+prints the following statistics:
 
+    - Total file size up to that point.
 
-def print_size_and_codes(size, stat_codes):
-    print("File size: {:d}".format(size))
-    for k, v in sorted(stat_codes.items()):
-        if v:
-            print("{:s}: {:d}".format(k, v))
+    - Count of read status codes up to that point.
 
+"""
 
-def parse_stdin_and_compute():
+def print_stats(size, status_codes):
+
+    """Print accumulated metrics.
+
+    Args:
+
+        size (int): The accumulated read file size.
+
+        status_codes (dict): The accumulated count of status codes.
+
+    """
+
+    print("File size: {}".format(size))
+
+    for key in sorted(status_codes):
+
+        print("{}: {}".format(key, status_codes[key]))
+
+if __name__ == "__main__":
+
+    import sys
+
     size = 0
-    lines = 0
-    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                  "403": 0, "404": 0, "405": 0, "500": 0}
+
+    status_codes = {}
+
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+
+    count = 0
+
     try:
+
         for line in sys.stdin:
-            fields = list(map(str, line.strip().split(" ")))
-            size += int(fields[-1])
-            if fields[-2] in stat_codes:
-                stat_codes[fields[-2]] += 1
-            lines += 1
-            if lines % 10 == 0:
-                print_size_and_codes(size, stat_codes)
+
+            if count == 10:
+
+                print_stats(size, status_codes)
+
+                count = 1
+
+            else:
+
+                count += 1
+
+            line = line.split()
+
+            try:
+
+                size += int(line[-1])
+
+            except (IndexError, ValueError):
+
+                pass
+
+            try:
+
+                if line[-2] in valid_codes:
+
+                    if status_codes.get(line[-2], -1) == -1:
+
+                        status_codes[line[-2]] = 1
+
+                    else:
+
+                        status_codes[line[-2]] += 1
+
+            except IndexError:
+
+                pass
+
+        print_stats(size, status_codes)
+
     except KeyboardInterrupt:
-        print_size_and_codes(size, stat_codes)
+
+        print_stats(size, status_codes)
+
         raise
-
-    print_size_and_codes(size, stat_codes)
-
-
-parse_stdin_and_compute()
