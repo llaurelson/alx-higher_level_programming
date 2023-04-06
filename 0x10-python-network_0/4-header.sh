9560579df1308e762718995b646ac2f3ee7096b9
llaurelson <llaurelson@gmail.com>
2023-04-06 14:51:54 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in URL, add header variable, displays "Hello Holberton School!"; Usage: ./4-header.sh 0.0.0.0:5000/route_5 ; echo ""
-curl -H "X-School-User-Id: 98" "http://0.0.0.0:5000/route_5/$1"
+curl -sH "X-School-User-Id: 98" "http://0.0.0.0:5000/route_5/$1"
